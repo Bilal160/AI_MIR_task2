ab3d62c60419b290223272112d44bfd854baccde
bilal160 <63717309+Bilal160@users.noreply.github.com>
2026-01-11 23:34:01 +0500

    updated deployed file

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,11 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
-    react(),
     tailwindcss(),
   ],
   build: {
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
+    outDir: 'dist',
   },
-  base: '/',
 })
